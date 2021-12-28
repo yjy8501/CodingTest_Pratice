5c17b848185962d711982d0e977458e6edcea0d0
Ahn <ajm153123@naver.com>
2021-12-28 16:02:04 +0900

    boj_1100 commit

--- src/algorigsm/BOJ_10869.java
+++ src/algorigsm/BOJ_10869.java
@@ -13,6 +13,24 @@ public class BOJ_10869 {
 		System.out.println(a*b);
 		System.out.println(a/b);
 		System.out.println(a%b);
-	}
+			
+		//BOJ_1100¹®Á¦
+		    char[][] chessBoard = new char[8][8];
+		
+		    int num = 0;
+			String str = "";
+		    for (int i = 0; i < 8; i++) {
+		      str = sc.nextLine();
+		      for (int j = 0; j < 8; j++) {
+		        chessBoard[i][j] = str.charAt(j);
 
+		        if ((i + j) % 2 == 0 && chessBoard[i][j] == 'F') {
+		            num++;
+		        }
+		      }
+		    }
+
+		    System.out.println(num);		
+		}
 }
+
