e977494bed393ae31f69b31a91dc1b356b047396
Ahn <ajm153123@naver.com>
2021-12-31 15:10:26 +0900

    BOJ_2739 commit

---
+++ src/algorigsm/BOJ_2739.java
@@ -0,0 +1,16 @@
+package algorigsm;
+
+import java.util.Scanner;
+
+public class BOJ_2739 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1;i<10;i++)
+			System.out.println(n+" * "+i+" = "+n*i);
+		sc.close();
+	}
+
+}
