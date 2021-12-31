ac61f2b398423bd3b85afa4d61f09560643dcf58
Ahn <ajm153123@naver.com>
2021-12-31 15:10:16 +0900

    BOJ_1008 commit

---
+++ src/algorigsm/BOJ_1008.java
@@ -0,0 +1,16 @@
+package algorigsm;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		
+		System.out.println(A/B);
+		sc.close();
+	}
+
+}
