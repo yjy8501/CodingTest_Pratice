d2062bf4e5e7db900ac6b1fabf718b6afa94753e
Ahn <ajm153123@naver.com>
2022-01-04 11:56:45 +0900

    BOJ_8393 commit - 1부터 n까지의 합

---
+++ src/algorigsm/BOJ_8393.java
@@ -0,0 +1,20 @@
+package algorigsm;
+
+import java.util.Scanner;
+
+public class BOJ_8393 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum = 0;
+		
+		for(int i = 1;i<n+1;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
