2cba2d1e531b2a997ccba551ef0ae84d1cfeeea8
Ahn <ajm153123@naver.com>
2022-01-06 14:58:04 +0900

    큰 수의 법칙 - Buffer

--- src/CodingTest/BigNumber_Buffer.java
+++ src/CodingTest/BigNumber_Buffer.java
@@ -11,33 +11,33 @@ public class BigNumber_Buffer {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
 		StringTokenizer st = new StringTokenizer(s);
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
-			int[] array = new int[n];
-			int sum = 0;
-			int index = 0;
+			int n = Integer.parseInt(st.nextToken()); // n 토큰 생성(배열 크기)
+			int m = Integer.parseInt(st.nextToken()); // m 토큰 생성(횟수)
+			int k = Integer.parseInt(st.nextToken()); // k 토큰 생성(규칙)
+			int[] array = new int[n]; // n크기의 배열 생성
+			int sum = 0; // 합계
+			int index = 0; // array배열 인덱스 번호
 			
-			StringTokenizer str = new StringTokenizer(br.readLine());
+			StringTokenizer str = new StringTokenizer(br.readLine()); 
 			 
-			while (str.hasMoreTokens()) {
-				array[index++] = Integer.parseInt(str.nextToken());
+			while (str.hasMoreTokens()) { // 토큰이 없을 때까지
+				array[index++] = Integer.parseInt(str.nextToken());//인덱스 1씩 증가하면서 정수 입력
 			}
-			Arrays.sort(array);
-			int back = array[n-1];
-			int back2 = array[n-2];
+			Arrays.sort(array);//배열 오름차순 정렬
+			int back = array[n-1];// 가장 큰 수
+			int back2 = array[n-2];// 두번째로 큰 수
 			
-			while(true) {
-				if(m == 0) break;
-				for(int i = 0;i<k;i++) {
+			while(true) { // 무한 반복
+				if(m == 0) break; // 횟수가 0이면 while문 탈출
+				for(int i = 0;i<k;i++) { //가장 큰 수를 k만큼 sum에 증가
 					sum += back;
-					m-=1;
-					if(m==0) break;
+					m-=1; // 횟수 감소
+					if(m==0) break; // 횟수가 0이면 while문 탈출
 				}
-				sum+=back2;
-				m-=1;
+				sum+=back2; // 두번째로 큰 수 sum에 증가
+				m-=1; // 횟수 감소
 			}
-			System.out.println(sum);
+			System.out.println(sum); // 횟수 출력
 	}
 
 }
